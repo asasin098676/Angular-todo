0e13f5bf1f20d549cff275235253472d7f1fd93a
Roman_Onyshko <asasin09867@gmail.com>
2022-06-28 16:34:43 +0300

    я правильно додаві сервіс???

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,20 @@ import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AddTextComponent } from './add-text/add-text.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+// import { HeroesComponent } from './heroes/heroes.component';
 
 const appRoutes: Routes = [
-{path: '', component: RegistrationComponent},
-{path:'add', component: AddTextComponent}
-] 
+  { path: '', component: RegistrationComponent },
+  { path: 'add', component: AddTextComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RegistrationComponent,
-    AddTextComponent
-  ],
+  declarations: [AppComponent, RegistrationComponent, AddTextComponent],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
